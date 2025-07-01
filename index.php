98522fc7f0a7b79ca67fbe5759f8aca705d21363
UmarFarooqKattak <umarfarooqweb1@gmail.com>
2025-07-01 11:31:11 +0500

    php modification

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me ||  Umar Farooq </title>
+    <link rel="icon" href="Umar Farooq.jpg">
+    <style>
+
+    </style>
+</head>
+<body>
+    <?php
+    echo "THIS IS THE FILE OF PHP ";
+    ?>
+</body>
+</html>
